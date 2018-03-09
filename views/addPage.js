b0724eae3167d9e4050cc916523e3c29f6b24417
Horacio Gutierrez <hogutier@yahoo.com>
2018-03-09 15:32:09 -0600

    html forms

--- views/addPage.js
+++ views/addPage.js
@@ -1,15 +1,26 @@
 const html = require("html-template-tag");
 const layout = require("./layout");
 
-module.exports = () => layout(html`
+module.exports = () =>
+  layout(html`
   <h3>Add a Page</h3>
   <hr>
   <form method="POST" action="/wiki/">
-    
-    <div>PLACEHOLDER FOR AUTHOR NAME FIELD</div>
-    
-    <div>PLACEHOLDER FOR AUTHOR EMAIL FIELD</div>
-    
+
+    <div class="form-group">
+      <label for="author" class="col-sm-2 control-label">Author</label>
+      <div class="col-sm-10">
+        <input id="author" name="author" type="text" class="form-control"/>
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label for="email" class="col-sm-2 control-label">Email</label>
+      <div class="col-sm-10">
+        <input id="email" name="email" type="text" class="form-control"/>
+      </div>
+    </div>
+
     <div class="form-group">
       <label for="title" class="col-sm-2 control-label">Page Title</label>
       <div class="col-sm-10">
@@ -17,13 +28,24 @@ module.exports = () => layout(html`
       </div>
     </div>
 
-    <div>PLACEHOLDER FOR PAGE CONTENT TEXTAREA FIELD</div>
-    
-    <div>PLACEHOLDER FOR PAGE STATUS INPUT FIELD</div>
+    <div class="form-group">
+      <label for="content" class="col-sm-2 control-label">Content</label>
+      <div class="col-sm-10">
+        <input id="content" name="content" type="text" class="form-control"/>
+      </div>content</div>
 
+    <div class="form-group">
+      <label for="page status" class="col-sm-2 control-label">Page status</label>
+      <div class="col-sm-10">
+        <select id="page status" name="page status"  class="form-control">
+        <option value="open">open</option>
+        <option value="closed">closed</option>
+        </select>
+
+      </div>content</div>page status
     <div class="col-sm-offset-2 col-sm-10">
       <button type="submit" class="btn btn-primary">submit</button>
     </div>
-  
+
   </form>
 `);
