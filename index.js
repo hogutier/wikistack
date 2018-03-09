ba93f7f1b77f62d405fc7950b1dceaa9da33efcc
Horacio Gutierrez <hogutier@yahoo.com>
2018-03-09 11:00:04 -0600

    Initial Server

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
+const PORT = 3000;
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/', (req, res, next) => {
+  res.send('hello world');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on Port ${PORT}`);
+})
