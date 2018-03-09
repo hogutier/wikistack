ca8ecb4e0e6ba9ae6fc57f648193c9876d0b04ca
Horacio Gutierrez <hogutier@yahoo.com>
2018-03-09 11:53:26 -0600

    connect to the database back-end

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const path = require('path');
-const PORT = 3000;
-const {main} = require('./views');
+const { main } = require('./views');
+const { db } = require('./models');
 
 const app = express();
+const PORT = 3000;
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -15,6 +16,9 @@ app.use('/', (req, res, next) => {
   res.send(main());
 });
 
-app.listen(PORT, () => {
-  console.log(`Listening on Port ${PORT}`);
-})
+db.authenticate().then(() => {
+  console.log('connected to the database');
+  app.listen(PORT, () => {
+    console.log(`Listening on Port ${PORT}`);
+  })
+});
